ec2116d193509463fb703b50e536e2453f1fead0
Your Username <ubaidullah2305@gmail.com>
2024-08-30 10:49:19 +0500

    First Commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://ubaid:ubaid2305@database.0jqj6.mongodb.net/?retryWrites=true&w=majority&appName=Database';
---
+++ backend/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow all origins with default of cors(*)
+app.use(cors());
+
+// Option 2: Allow custom origins
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("MERN Server is running");
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
